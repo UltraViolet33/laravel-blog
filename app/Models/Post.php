7273dc414868051d6ce9a234b4f0a2003bfbf538
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-09 09:06:51 +0100

    use blade component

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -40,11 +40,8 @@ class Post
 
     public static function find($slug)
     {
+        $posts = static::all();
 
-        if (!file_exists($path = resource_path("/posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
+        return $posts->firstWhere("slug", $slug);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Blog</title>
-    <link rel="stylesheet" href="./style.css">
-</head>
-
-<body>
-    <?php foreach ($posts as $post): ?>
-    <article>
-    <h1>
-        <a href="posts/<?=$post->slug?>">
-        <?= $post->title ?></a></h1>
-    <div>
-        <p><?= $post->excerpt ?></p>
-    </div>
-    </article>
-    <?php endforeach;?>
-</body>
-
-</html>
+<x-layout>
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="posts/{{ $post->slug }}">
+                    {{ $post->title }} </a>
+            </h1>
+            <div>
+                <p>{{ $post->excerpt }}</p>
+            </div>
+        </article>
+    @endforeach
+</x-layout>
