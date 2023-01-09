7b74c9722a0fb3c342d65daaa5bd2106e466069c
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-09 09:48:53 +0100

    start migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,55 +1,11 @@
 <?php
 
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post
+class Post extends Model
 {
-    public function __construct(
-        public string $title,
-        public string $excerpt,
-        public string $date,
-        public string $body,
-        public string $slug
-    ) {
-    }
-
-    public static function all()
-    {
-        return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path("posts")))
-                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-                ->map(fn ($document) => new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-                ))->sortByDesc('date');
-        });
-    }
-
-
-    public static function find($slug)
-    {
-        $post = static::all()->firstWhere("slug", $slug);
-        return $post;
-    }
-
-
-    public static function findOrFail($slug)
-    {
-        $post = static::find($slug);
-
-        if (!$post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
+    use HasFactory;
 }
