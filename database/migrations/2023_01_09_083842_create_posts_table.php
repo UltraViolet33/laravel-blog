a9aede2df192734c6e6e08c6bd0ad19c5e411cd6
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-06-19 09:23:59 +0200

    start category controller

--- database/migrations/2023_01_09_083842_create_posts_table.php
+++ database/migrations/2023_01_09_083842_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("category_id");
+            $table->foreignIdFor(Category::class);
             $table->string("title");
             $table->string("slug")->unique();
             $table->text("body");
